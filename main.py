71796ce0609558a9c137c1495bc0c3ae03dd35b4
HEVNz <e-exe-e@live.com>
2022-05-01 04:06:02 +0700

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+from iqoptionapi.stable_api import IQ_Option
+from dotenv import load_dotenv
+import os
+load_dotenv()
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+bot = IQ_Option(email, password)
+bot.connect()
+
+while True:
+    if bot.connect():
+        print("Connected")
+        break
+    else:
+        print("Failed to connect")
+
+print("I am Online!")
