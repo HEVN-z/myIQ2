5f1c34028d9766ff3475a57884b77b2892146e0a
HEVNz <e-exe-e@live.com>
2022-05-01 12:57:04 +0700

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from iqoptionapi.stable_api import IQ_Option
 from dotenv import load_dotenv
 import os
@@ -16,3 +17,8 @@ while True:
         print("Failed to connect")
 
 print("I am Online!")
+
+while True:
+    indi = bot.get_technical_indicators("EURUSD")
+    print(indi)
+    time.sleep(.2)
