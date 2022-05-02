0d6ac66b01e1d9a28c57796211ce7a9766ae5ba3
HEVNz <e-exe-e@live.com>
2022-05-02 19:16:08 +0700

    test gui

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
+from functools import cache
 import time
 from iqoptionapi.stable_api import IQ_Option
 from dotenv import load_dotenv
 import os
+import json
 load_dotenv()
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
 bot = IQ_Option(email, password)
+'''
 bot.connect()
 
 while True:
@@ -17,8 +20,30 @@ while True:
         print("Failed to connect")
 
 print("I am Online!")
+'''
+dict1 ={
+    "emp1": {
+        "name": "Lisa",
+        "designation": "programmer",
+        "age": "34",
+        "salary": "54000"
+    },
+    "emp2": {
+        "name": "Elis",
+        "designation": "Trainee",
+        "age": "24",
+        "salary": "40000"
+    }
+}
+dict1['emp3'] = {'name':"HEVNz"}
+try:
+    r = json.load(open("_json.json"))
+except:
+    pass
+w = open("_json.json", "w")
+json.dump(dict1, w, indent=4)
 
-while True:
-    indi = bot.get_technical_indicators("EURUSD")
-    print(indi)
-    time.sleep(.2)
+
+
+print(r['emp2'])
+#while True:
---
+++ testgui.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+def set_lb():
+    i = int(w.get())
+    if i > 10:
+        i = 10
+    elif i < 0:
+        i = 0
+    lb.configure(text = i)
+
+
+
+master = Tk()
+#master.state('zoomed')
+master.resizable(0,0)
+master.geometry("400x400")
+master.attributes("-topmost", True)
+t_checker = False
+
+def new_win():
+    def close():
+        global t_checker
+        t_checker = False
+        t.destroy()
+    global t_checker
+    if t_checker:
+        return
+    t_checker = True
+    t = Toplevel(master)
+    t.wm_resizable(width=False, height=False)
+    t.title("New window")
+    t.transient(master)
+    t.focus()
+    t.protocol("WM_DELETE_WINDOW", lambda: close())
+
+w = Spinbox(master, from_=0, to=10)
+w.pack()
+
+bt = Button(master, text="OK", command=new_win)
+bt.pack()
+
+lb = Label(master, text="This is label")
+lb.pack()
+
+mainloop()
