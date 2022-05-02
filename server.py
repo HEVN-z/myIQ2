1041e1f8205ef9a790a5dae8c589aba8562543b2
HEVNz <e-exe-e@live.com>
2022-05-03 03:37:09 +0700

    up test

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from functools import cache
 import time
 from iqoptionapi.stable_api import IQ_Option
 from dotenv import load_dotenv
@@ -9,8 +8,14 @@ email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
 bot = IQ_Option(email, password)
-'''
 bot.connect()
+
+while True:
+    print(bot.get_balance())
+    print(bot.get_balance_mode())
+    print(bot.get_balance_id())
+    time.sleep(1)
+'''
 
 while True:
     if bot.connect():
