cd361a913077fe58e50101f87c81bc2a2e934dbd
HEVNz <e-exe-e@live.com>
2022-05-05 19:20:14 +0700

    g

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 from iqoptionapi.stable_api import IQ_Option
 from dotenv import load_dotenv
+import server.server_gui as gui
+print("Loading .env file...")
 import os
 import json
 load_dotenv()
@@ -9,9 +11,11 @@ password = os.getenv("PASSWORD")
 
 bot = IQ_Option(email, password)
 bot.connect()
+gui.mainloop()
 
 while True:
     print(bot.get_balance())
     print(bot.get_balance_mode())
     print(bot.get_balance_id())
     time.sleep(1)
+
